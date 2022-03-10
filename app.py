c2d7f17c964d068e30255902652070945dabf665
Joran Le Quellec <joran.lequellec@gmail.com>
2022-03-11 09:25:26 +1300

    default pfp, signin and signup error check

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, g, request, redirect, url_for, session
 import sqlite3
 import time
+import hashlib
 
 DATABASE = "users.db"
 
@@ -25,16 +26,33 @@ def close_connection(exception):
 
 @app.get("/")
 def home():
-    cursor = get_db().cursor()
-    sql = "SELECT * FROM users"
-    cursor.execute(sql)
-    results = cursor.fetchall()
     if 'username' in session:
-        if username in results:
-            return render_template('home.html')
+        cursor = get_db().cursor()
+        sql = "SELECT username FROM users"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for i in range(len(results)):
+            results[i] = results[i][0]
+        if session['username'] in results:
+            cursor = get_db().cursor()
+            sql = "SELECT password FROM users"
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            for i in range(len(results)):
+                results[i] = results[i][0]
+            if session['password'] in results:
+                cursor = get_db().cursor()
+                sql = "SELECT * FROM users WHERE username = ?"
+                cursor.execute(sql, (session['username'], ))
+                results = cursor.fetchall()
+                print(results[0][3])
+                return render_template('home.html', userData=results)
+            else:
+                errorMessage = "Incorrect password, please try again."
+                return render_template('signin.html', errorMessage=errorMessage)
         else:
-            errorMessage = "Username does not exist. Please signup."
-            return render_template('error.html', errorMessage=errorMessage)
+            errorMessage = "Username does not exist."
+            return render_template('signin.html', errorMessage=errorMessage)
     else:
         errorMessage = "You are not logged in."
         return render_template('error.html', errorMessage=errorMessage)
@@ -44,19 +62,30 @@ def home():
 def home_post():
     username = request.form['username']
     password = request.form['password']
+    h = hashlib.md5(password.encode())
+    password = h.hexdigest()
     cursor = get_db().cursor()
-    sql = "INSERT INTO users(username,password) VALUES(?,?)"
-    cursor.execute(sql, (username, password))
-    get_db().commit()
+    sql = "SELECT username FROM users"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for i in range(len(results)):
+        results[i] = results[i][0]
+    if username not in results:
+        cursor = get_db().cursor()
+        sql = "INSERT INTO users(username,password, pfp) VALUES(?,?,?)"
+        cursor.execute(sql, (username, password, "/static/pfps/default.png"))
+        get_db().commit()
+        session['username'] = username
+        session['password'] = password
+        return redirect(url_for('home'))
+    else: 
+        errorMessage = "Username is already in use, please choose something else."
+        return render_template('signup.html', errorMessage=errorMessage)
 
 
 @app.route("/signup")
 def signup():
-    cursor = get_db().cursor()
-    sql = "SELECT * FROM users"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template('signup.html', results=results)
+    return render_template('signup.html')
 
 
 @app.route("/account")
@@ -72,12 +101,14 @@ def account():
 def signin():
     if request.method == 'POST':
         session['username'] = request.form['username']
-        username = request.form['username']
-        return redirect(url_for('home', username=username))
+        password = request.form['password']
+        h = hashlib.md5(password.encode())
+        session['password'] = h.hexdigest()
+        return redirect(url_for('home'))
     return render_template('signin.html')
 
 
-@app.route('/logout')   
+@app.route('/logout')
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
